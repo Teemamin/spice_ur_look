8ca946c7ecbeebfd8cce1ef29e002dca27045951
Fatima Aminu <teemamin@yahoo.com>
2020-09-26 12:50:03 +0000

    search functionality added to products view

--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from .models import Product, Category
+from django.contrib import messages
+from django.db.models import Q
 
 # Create your views here.
 
@@ -10,8 +12,18 @@ def all_products(request):
         if 'gender' in request.GET:
             gender = request.GET['gender']
             products = products.filter(gender=gender)
+        if 'search' in request.GET:
+            search_word = request.GET['search']
+            if not search_word:
+                messages.error(request, "please enter a message")
+                return redirect(reverse('products'))
+            search_words = Q(name__icontains=search_word) |\
+                Q(description__icontains=search_word)\
+                | Q(color__icontains=search_word)
+            products = products.filter(search_words)
     context = {
         'products': products,
+        'search_word': search_word,
 
     }
     return render(request, 'products/products.html', context)
