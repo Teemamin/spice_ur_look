d5c778bbdac70ae18f61ecf6c1e04f5ba7eef1f3
Fatima Aminu <teemamin@yahoo.com>
2020-10-13 08:03:46 +0000

    social login setup settings testing on heroku deployed app

--- spice_ur_look/settings.py
+++ spice_ur_look/settings.py
@@ -127,7 +127,7 @@ SOCIALACCOUNT_PROVIDERS = {
             'email',
         ],
         'AUTH_PARAMS': {
-            'access_type': 'offline',
+            'access_type': 'online',
         }
     }
 }
