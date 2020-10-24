0e6a3b9a0ccdaecdce8e35f474611194f848bf69
Fatima Aminu <teemamin@yahoo.com>
2020-10-24 16:50:03 +0000

    debugging in heroku

--- spice_ur_look/settings.py
+++ spice_ur_look/settings.py
@@ -121,21 +121,8 @@ ACCOUNT_USERNAME_MIN_LENGTH = 5
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 
-# SOCIALACCOUNT_PROVIDERS = {
-#     'google': {
-#         'SCOPE': [
-#             'profile',
-#             'email',
-#         ],
-#         'AUTH_PARAMS': {
-#             'access_type': 'online',
-#         }
-#     }
-# }
 
 WSGI_APPLICATION = 'spice_ur_look.wsgi.application'
-
-
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
